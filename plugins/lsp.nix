5936b3b4d43b3537c6fefc5474cead7622592d2f
HappyOri <orkhan.tahirov@gmail.com>
2024-05-05 23:47:39 -0700

    Added basic lsp servers

--- plugins/lsp.nix
+++ plugins/lsp.nix
@@ -7,6 +7,14 @@
         enable = true;
         package = inputs.nixd.packages.${system}.nixd;
       };
+      lua-ls = {};
+      dockerls = {};
+      jsonls = {};
+      nushell = {};
+      rust-analyzer = {};
+      tsserver = {};
+      yamlls = {};
+      gopls = {};
     };
   };
 }
