5cafcfc0c5ac1ccc2e4691cdaf9495d349a9d41f
HappyOri <orkhan.tahirov@gmail.com>
2024-05-06 00:01:58 -0700

    Added neodev

--- plugins/lsp.nix
+++ plugins/lsp.nix
@@ -1,4 +1,4 @@
-{ inputs, system, ... }:
+{ inputs, pkgs, system, ... }:
 {
   plugins.lsp = {
     enable = true;
@@ -7,14 +7,38 @@
         enable = true;
         package = inputs.nixd.packages.${system}.nixd;
       };
-      lua-ls = {};
-      dockerls = {};
-      jsonls = {};
-      nushell = {};
-      rust-analyzer = {};
-      tsserver = {};
-      yamlls = {};
-      gopls = {};
+      lua-ls = { };
+      dockerls = { };
+      jsonls = { };
+      nushell = { };
+      rust-analyzer = { };
+      tsserver = { };
+      yamlls = { };
+      gopls = { };
     };
   };
+
+  extraPlugins = [
+    pkgs.vimPlugins.neodev-nvim
+  ];
+
+  extraConfigLua = /* lua */ ''
+    require("neodev").setup({
+      library = {
+        enable = true,
+        runtime = true,
+        types = true,
+        plugins = { "nvim-treesitter", "telescope.nvim" },
+      },
+      setup_jsonls = true,
+      lspconfig = true,
+      pathStrict = true,
+      override = function(root_dir, lib)
+        if root_dir:find("${./.}/lua", 1, true) == 1 then
+          lib.enabled = true
+          lib.plugins = true
+        end
+      end
+    })
+  '';
 }
